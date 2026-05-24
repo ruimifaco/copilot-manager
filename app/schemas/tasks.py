5dd72c527ab51aa6cefa0f1d3c92a1fa73296da4
Rui Facó <rui.mfaco@gmail.com>
2026-05-23 21:23:42 -0300

    feat: adiciona schema para tasks

---
+++ app/schemas/tasks.py
@@ -0,0 +1,14 @@
+from datetime import date
+from pydantic import BaseModel
+from datetime import time
+
+class TaskCreate(BaseModel):
+    title: str
+    task_date: date
+    planned_start_time: time
+    planned_end_time: time
+    actual_start_time: time
+    actual_end_time: time
+    final_status: str
+    user_id: int
+    category_id: int
